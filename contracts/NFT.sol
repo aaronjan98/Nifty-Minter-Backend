65bb3c259abd5733e38f4b564b9561f47d9516ff
aaronjan98 <aaronjan98@gmail.com>
2023-01-18 10:05:32 -0800

    add function overrides required by Solidity

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+// import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import 'hardhat/console.sol';
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    using Strings for uint256;
 
     constructor(
         string memory tokenName,
@@ -22,5 +26,50 @@ contract NFT is ERC721URIStorage {
         _setTokenURI(id, _tokenURI);
 
         return id;
+    }
+
+    function walletOfOwner(
+        address _owner
+    ) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function freeMint(address to, string memory nftTokenURI) public {
+        _safeMint(to, _tokenIds.current());
+        _setTokenURI(_tokenIds.current(), nftTokenURI);
+        _tokenIds.increment();
+    }
+
+    // Overrides required by Solidity
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        return super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
     }
 }
