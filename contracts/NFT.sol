c4090e5d601415ad51ccd7bf1095fc08da24a733
aaronjan98 <aaronjan98@gmail.com>
2023-01-19 14:47:03 -0800

    create deploy script

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import 'hardhat/console.sol';
 
-contract NFT is ERC721URIStorage, ERC721Enumerable, Ownable {
+contract NFT is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     using Strings for uint256;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,28 +4,52 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat')
+const { ether } = require('../common/tokens.js')
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NAME = 'Mayday'
+  const SYMBOL = 'MD'
+  const fee = ether(0.01)
+  let URI =
+    'https://ai-gen-nft-minter.infura-ipfs.io/ipfs/QmQQbJ4iCcWzJjpSi2QrAv57gyXFxDkDmbGcWJkeqA7zXY/'
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  let nft, deployer, minter
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Deploy Token
+  const NFT = await ethers.getContractFactory('NFT')
+  nft = await NFT.deploy(NAME, SYMBOL, fee)
 
-  await lock.deployed();
+  await nft.deployed()
+  console.log(`\nNFT deployed to: ${nft.address}\n`)
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // save contract ABI to front end
+  saveABI(nft)
+}
+
+function saveABI(nft) {
+  const fs = require('fs')
+  const abi = artifacts.readArtifactSync('NFT').abi
+  const filePath = '../frontend/src/abis/NFT.json'
+
+  // check if the file already exists
+  if (!fs.existsSync(filePath)) {
+    // create the file if it doesn't exist
+    fs.openSync(filePath, 'w')
+  }
+
+  fs.writeFile(filePath, JSON.stringify(abi, null, 2), 'utf8', err => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('ABI saved to frontend directory')
+    }
+  })
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
